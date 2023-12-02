ea05dfa8c4aa57e22f67766d81930ad7c37c184d
Stéphane Henriot <shenriot.pro@gmail.com>
2023-12-02 22:48:07 +0100

    refactor day01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,67 +1,54 @@
 use std::fs;
 
 fn part1(input: &str) -> i32 {
-    let mut res = 0;
-    for line in input.lines() {
-        let digits: Vec<_> = line.chars().filter(|c| c.is_ascii_digit()).collect();
-        // Note: first and last digits may be the same.
-        let mut value = String::new();
-        value.push(*digits.first().expect("Should have digits"));
-        value.push(*digits.last().expect("Should have digits"));
-        let value = value.parse::<i32>().expect("Should be an integer");
-        res += value;
-    }
-    res
+    input
+        .lines()
+        .map(|line| {
+            let digits: Vec<_> = line.chars().filter(|c| c.is_ascii_digit()).collect();
+            // Note: first and last digits may be the same.
+            let first = *digits.first().expect("Should have digits");
+            let last = *digits.last().expect("Should have digits");
+            format!("{first}{last}")
+                .parse::<i32>()
+                .expect("Should be an integer")
+        })
+        .sum()
 }
 
 fn part2(input: &str) -> i32 {
-    let mut res = 0;
     let hays = [
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
         "seven", "eight", "nine",
     ];
-    for line in input.lines() {
-        // Note: digits may overlap ("twone"), we can't trvially rewrite them.
-        // But there should always be a clear first and last (no prefix/suffix).
-        // Regexes don't seem to work well with overlapping matches.
-        // "zero" may not be a digit?
-        let mut first: Option<&str> = None;
-        let mut first_idx: Option<usize> = None;
-        for hay in hays {
-            if let Some(idx) = line.find(hay) {
-                if first_idx.is_none() || idx < first_idx.unwrap() {
-                    first_idx = Some(idx);
-                    first = Some(hay);
-                }
-            }
-        }
-        let mut last: Option<&str> = None;
-        let mut last_idx: Option<usize> = None;
-        for hay in hays {
-            if let Some(idx) = line.rfind(hay) {
-                if last_idx.is_none() || idx > last_idx.unwrap() {
-                    last_idx = Some(idx);
-                    last = Some(hay);
-                }
-            }
-        }
-        let mut value = String::new();
-        value.push_str(first.expect("Should have digits"));
-        value.push_str(last.expect("Should have digits"));
-        let value = value
-            .replace("one", "1")
-            .replace("two", "2")
-            .replace("three", "3")
-            .replace("four", "4")
-            .replace("five", "5")
-            .replace("six", "6")
-            .replace("seven", "7")
-            .replace("eight", "8")
-            .replace("nine", "9");
-        let value = value.parse::<i32>().expect("Should be an integer");
-        res += value;
-    }
-    res
+    input
+        .lines()
+        .map(|line| {
+            // Note: digits may overlap ("twone"), we can't trvially rewrite them.
+            // But there should always be a clear first and last (no prefix/suffix).
+            // Regexes don't seem to work well with overlapping matches.
+            // "zero" may not be a digit?
+            let first = *hays
+                .iter()
+                .min_by_key(|hay| line.find(*hay).unwrap_or(usize::MAX))
+                .expect("Should find digits");
+            let last = *hays
+                .iter()
+                .max_by_key(|hay| line.rfind(*hay).unwrap_or(usize::MIN))
+                .expect("Should find digits");
+            format!("{first}{last}")
+                .replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+                .parse::<i32>()
+                .expect("Should be an integer")
+        })
+        .sum()
 }
 
 fn main() {
