df0a6154881dd9f30e5f2ae87eae190677557a55
Stéphane Henriot <shenriot.pro@gmail.com>
2023-12-12 17:57:06 +0100

    add day12 (needs refactoring)

---
+++ src/bin/day12.rs
@@ -0,0 +1,272 @@
+use std::fs;
+
+use cached::proc_macro::cached;
+use itertools::Itertools;
+
+struct Record {
+    mask: String,
+    groups: Vec<i64>,
+}
+
+fn part1(input: &str) -> i64 {
+    let records = input.lines().map(parse_line).collect_vec();
+    records.iter().map(combinations).sum()
+}
+
+fn parse_line(line: &str) -> Record {
+    let (mask, groups) = line
+        .split_whitespace()
+        .collect_tuple()
+        .expect("Should have two parts");
+    let mask = mask.to_string();
+    let groups = groups
+        .split(',')
+        .map(|e| e.parse::<i64>().expect("Should be a number"))
+        .collect_vec();
+    Record { mask, groups }
+}
+
+#[cached]
+fn combinations2(mask: String, groups: Vec<i64>) -> i64 {
+    match mask.find('?') {
+        None => {
+            if matches_exactly(&mask, &groups) {
+                1
+            } else {
+                0
+            }
+        }
+        Some(position) => {
+            match mask
+                .get(..position)
+                .expect("Should be a valid slice")
+                .rfind('.')
+            {
+                None => {
+                    let mut res = 0;
+                    for c in ['.', '#'] {
+                        let mut new_mask = mask.to_owned();
+                        let groups = groups.clone();
+                        new_mask.replace_range(position..position + 1, &c.to_string());
+                        if matches_partially(&new_mask, &groups) {
+                            res += combinations2(new_mask, groups);
+                        }
+                    }
+                    res
+                }
+                Some(last) => {
+                    let start_groups =
+                        compute_groups(mask.get(..last).expect("Should be a valid slice"));
+                    if groups.starts_with(&start_groups) {
+                        combinations2(
+                            mask.get(last + 1..)
+                                .expect("Should be a valid slice")
+                                .to_string(),
+                            groups[start_groups.len()..].to_vec(),
+                        )
+                    } else {
+                        0
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn combinations(record: &Record) -> i64 {
+    let r = combinations_backtrack(&record.mask, 0, &record.groups);
+    println!("{}: {}", record.mask, r);
+    r
+}
+
+fn combinations_backtrack(mask: &String, position: usize, groups: &[i64]) -> i64 {
+    // TODO: we should probably check chars' len
+    if position == mask.len() {
+        if matches_exactly(mask, groups) {
+            1
+        } else {
+            0
+        }
+    } else if mask
+        .chars()
+        .nth(position)
+        .expect("Should be a valid position")
+        != '?'
+    {
+        combinations_backtrack(mask, position + 1, groups)
+    } else {
+        let mut res = 0;
+        for c in ['.', '#'] {
+            let mut new_mask = mask.clone();
+            new_mask.replace_range(position..position + 1, &c.to_string());
+            if matches_partially(&new_mask, groups) {
+                res += combinations_backtrack(&new_mask, position + 1, groups);
+            }
+        }
+        res
+    }
+}
+
+fn matches_partially(mask: &str, groups: &[i64]) -> bool {
+    // if mask.contains('#') {
+    //     let max_group = groups.iter().max().expect("Should have a max");
+    //     // TODO: we should check there is a #
+    //     let current_max = mask
+    //         .chars()
+    //         .group_by(|&c| c)
+    //         .into_iter()
+    //         .filter(|(c, _)| *c == '#')
+    //         .map(|(_, g)| g.count())
+    //         .max()
+    //         .expect("Should have a max");
+    //     if (current_max as i64) > *max_group {
+    //         return false;
+    //     }
+    // }
+    let mask_groups = compute_partial_groups(mask);
+    if mask_groups.is_empty() {
+        return true;
+    }
+    let mut found = 0;
+    for g in groups {
+        if mask_groups[found] == *g {
+            found += 1;
+            if found == mask_groups.len() {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn compute_partial_groups(mask: &str) -> Vec<i64> {
+    let mut res = vec![];
+    let mut current = 0;
+    let mut valid = true;
+    for c in mask.chars() {
+        match c {
+            '#' => current += 1,
+            '.' => {
+                if current > 0 && valid {
+                    res.push(current);
+                }
+                current = 0;
+                valid = true;
+            }
+            '?' => {
+                current = 0;
+                valid = false;
+            }
+            _ => panic!("Should be a valid mask"),
+        }
+    }
+    if current > 0 && valid {
+        res.push(current);
+    }
+    res
+}
+
+fn matches_exactly(mask: &str, groups: &[i64]) -> bool {
+    let mask_groups = compute_groups(mask);
+    mask_groups == groups
+}
+
+fn compute_groups(mask: &str) -> Vec<i64> {
+    let mut res = vec![];
+    let mut current = 0;
+    for c in mask.chars() {
+        match c {
+            '#' => current += 1,
+            '.' => {
+                if current > 0 {
+                    res.push(current);
+                    current = 0;
+                }
+            }
+            _ => panic!("Should be a filled mask"),
+        }
+    }
+    if current > 0 {
+        res.push(current);
+    }
+    res
+}
+
+fn part2(input: &str) -> i64 {
+    let records = input.lines().map(parse_line2).collect_vec();
+    records
+        .iter()
+        .map(|record| {
+            println!("{}", record.mask);
+            combinations2(record.mask.clone(), record.groups.clone())
+        })
+        .sum()
+}
+
+fn parse_line2(line: &str) -> Record {
+    let rec = parse_line(line);
+    let mask5 = format!(
+        "{}?{}?{}?{}?{}",
+        rec.mask, rec.mask, rec.mask, rec.mask, rec.mask
+    );
+    let groups5 = [
+        rec.groups.clone(),
+        rec.groups.clone(),
+        rec.groups.clone(),
+        rec.groups.clone(),
+        rec.groups,
+    ]
+    .iter()
+    .flatten()
+    .cloned()
+    .collect_vec();
+    Record {
+        mask: mask5,
+        groups: groups5,
+    }
+}
+
+fn main() {
+    let file_path = "data/day12_input.txt";
+
+    let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+
+    #[test]
+    fn test_part1() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+
+        assert_eq!(part1(input), 21);
+    }
+
+    #[test]
+    fn test_part1_basic() {
+        let input = ".??..??...?##. 1,1,3";
+
+        assert_eq!(part1(input), 4);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+
+        assert_eq!(part2(input), 525152);
+    }
+}
